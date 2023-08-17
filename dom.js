7aaa1d8cd8003715a7f777072fbbb194d2a9fff0
Anurag sharma <141912665+Anuragsrma1@users.noreply.github.com>
2023-08-17 12:55:11 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-var headerTitle = document.getElementById('header-title');
+/* var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('mainheader');
 
 var items = document.getElementsByClassName('list-group-item');
@@ -8,5 +8,19 @@ items[2].textContent = 'jon';
 items[2].style.frontweight = 'bold';
 items[2].style.background = 'green';
 
+for(var i=0 ; i<items.length; i++){
+    items[i].style.background = '#f4f4f4';
+} */
 
+//GETELEMENTSBYTAGNAME 
 
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[2].textContent = 'jon';
+li[2].style.frontweight = 'bold';
+li[2].style.background = 'green';
+
+for(var i=0 ; i<li.length; i++){
+    li[i].style.background = '#f4f4f4';
+}
