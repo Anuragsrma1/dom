4677749dac2f172d071da9f46c1666ef4a79ad02
Anurag sharma <141912665+Anuragsrma1@users.noreply.github.com>
2023-08-17 11:17:29 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,7 +1,12 @@
 var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('mainheader');
 
-
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].textContent = 'jon';
+items[2].style.frontweight = 'bold';
+items[2].style.background = 'green';
 
 
 
