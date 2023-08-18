493239387633439f555e1f6a45f5e5541669b005
Anurag sharma <141912665+Anuragsrma1@users.noreply.github.com>
2023-08-18 11:54:08 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -14,7 +14,7 @@ for(var i=0 ; i<items.length; i++){
 
 //GETELEMENTSBYTAGNAME 
 
-var li = document.getElementsByTagName('li');
+/* var li = document.getElementsByTagName('li');
 console.log(li);
 console.log(li[1]);
 li[2].textContent = 'jon';
@@ -23,4 +23,36 @@ li[2].style.background = 'green';
 
 for(var i=0 ; i<li.length; i++){
     li[i].style.background = '#f4f4f4';
+}
+ */
+
+// queryselector
+/* var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Enter text';
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color= 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = 
+document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color ='coral'; */
+
+// QUERYSELECTORALL //
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent= 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+  
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
 }
