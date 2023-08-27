fcf531d4848b02be34a0bd2d5a5a35aa89c9a85b
Anurag sharma <141912665+Anuragsrma1@users.noreply.github.com>
2023-08-27 11:47:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,26 @@ function saveToLocalStorage(event){
     const password = event.target.password.value;
     const birthdate = event.target.birthdate.value;
     const gender = event.target.gender.value;
-    localStorage.setItem('fullname',name);
+   /*  localStorage.setItem('fullname',name);
     localStorage.setItem('email',email);
     localStorage.setItem('password',password);
     localStorage.setItem('birthdate',birthdate);
-    localStorage.setItem('gender',gender);
+    localStorage.setItem('gender',gender); */
 
     const obj = {
-      fullname,
+      name,
       email,
       password,
       birthdate,
       gender
    }
-   localStorage.setItem('userdetails',JSON.stringify(obj));
-
-
+   localStorage.setItem(obj.email,JSON.stringify(obj));
+    showUserOnScreen(obj)
 }
+
+ function showUserOnScreen(obj){
+    const parentElem = document.getElementById('listOfItems')
+
+    parentElem.innerHTML = `<li>${obj.name} - ${obj.email} - ${obj.password} - ${obj.birthdate} - ${obj.gender}</li>`  
+       
+   }
